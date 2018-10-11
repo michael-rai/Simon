7a4a9d7fc7262699fa12fa351393a7ec1daa4021
michael-rai <michaelrai.prgmr@gmail.com>
2018-10-11 15:20:14 -0400

    general layout acheived

---
+++ simon_js.js
@@ -0,0 +1,12 @@
+var colorArray = ['yellow','blue','white','deeppink',]
+
+document.getElementById("button").addEventListener("click", runColorSequence);
+	console.log="run button clicked";
+
+var runColorSequence = function (){
+	var colorbox = document.getElementByClassName('colorbox');
+	console.log="function working";
+};
+
+
+
