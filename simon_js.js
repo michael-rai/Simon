ba1fddc3b7b5acf8b56aa04e4dc8c4953fc41759
michael-rai <michaelrai.prgmr@gmail.com>
2018-12-12 00:32:41 -0500

    deployment

--- simon_js.js
+++ simon_js.js
@@ -1,12 +1,31 @@
+var alertUser = function (){
+	alert('This is a test notification to advise JS is sucessfully loaded and will be removed in final version');
+
+};
+
+alertUser();
+
+
 var colorArray = ['yellow','blue','white','deeppink',]
 
 document.getElementById("button").addEventListener("click", runColorSequence);
 	console.log="run button clicked";
 
-var runColorSequence = function (){
+document.getElementById("button1").addEventListener("click", colorClicked);
+
+document.getElementById("button2").addEventListener("click", colorClicked);
+
+document.getElementById("button3").addEventListener("click", colorClicked);
+
+document.getElementById("button4").addEventListener("click", colorClicked);
+
+/*var runColorSequence = function (){
 	var colorbox = document.getElementByClassName('colorbutton');
 	console.log="function working";
 };
+*/
+
+var colorClicked = function {}
 
 
 
